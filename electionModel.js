10965f87df5d34f7f61fcbeb240e1eef60b27140
Vivian Luxon <arterinv@gmail.com>
2021-04-06 13:31:23 +1200

    edited, added mocha and chai

--- electionModel.js
+++ electionModel.js
@@ -5,7 +5,7 @@ const STORAGE_KEY = 'candidates'
 
 // FEATURE 2. Add a part.
 class Candidate{
-    constructor(newPartyName, newCandidateName, newVotes = 0) {
+    constructor(newCandidateName, newPartyName, newVotes = 0) {
         this.candidateName = newCandidateName
         this.partyName = newPartyName
         this.votes = newVotes // FEATURE 13. Provide default values.
@@ -18,14 +18,17 @@ class Electorate {
     constructor (newElectorateName) {
         this.electorateName = newElectorateName
         this.candidates = []
-        this.candidateCount = this.candidates.length
+        // Following attributes are for supporting editing candidateName
+        this.editedCandidateIndex = null
+        this.beforeEditNameCache = ''
+
     }
 
     // FEATURE 2. Add a part.
-    setNewCandidate (newPartyName, newCandidateName, newVotes) {
+    setNewCandidate (newCandidateName, newPartyName, newVotes) {
         if (!this.candidates.some(i => i.candidateName === newCandidateName)) {
             // The candidate doesn't exist in the electorate, so add them. candidateName is unique.
-            var newCandidate = new Candidate(newPartyName, newCandidateName, newVotes)
+            var newCandidate = new Candidate(newCandidateName, newPartyName, newVotes)
             this.candidates.push(newCandidate)
         }
     }
@@ -33,6 +36,7 @@ class Electorate {
     // FEATURE 3. Sort parts.
     // TODO clean this mess up, not one line.
     sortCandidatesByVoteCount () {
+        // Sorts high to low by votes, sorts alphabetically by name if tied on votes.
         return this.candidates.sort((a, b) => (a.votes > b.votes) ? -1 : (a.votes === b.votes) ? ((a.candidateName > b.candidateName) ? 1 : -1) : 1)
     }
 
@@ -48,17 +52,42 @@ class Electorate {
     }
 
     //  FEATURE 6. Save all parts to LocalStorage.
-    save () {
+    saveCandidates () {
         localStorage.setItem(STORAGE_KEY, JSON.stringify(this.candidates))
     }
 
     // FEATURE 7. Load all parts from LocalStorage.
     // FEATURE 13. Provide default values.
-    load () {
+    loadCandidates () {
         return JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]')
     }
 
-    // TODO editing
+    // FEATURE 8. Update/edit a part. TODO validate input
+    startEditingCandidateName (toBeEditedCandidate) {
+        this.editedCandidateIndex = this.candidates.findIndex(i => i.candidateName === toBeEditedCandidate)
+        this.beforeEditNameCache = toBeEditedCandidate
+    }
+
+    // FEATURE 8. Update/edit a part.
+    finishEditingCandidateName (newCandidateName) {
+        if (!this.candidates.some(i => i.candidateName === newCandidateName)) {
+            this.candidates[this.editedCandidateIndex].candidateName = newCandidateName
+        }
+    }
+
+    // FEATURE 8. Update/edit a part.
+    cancelEditingCandidateName () {
+        this.editedCandidateIndex = null
+        this.editedCandidateIndex = null
+    }
+
+    // FEATURE 8. Update/edit a part.
+    revertEditingCandidateName () {
+        // Only reverts last name edited
+        this.candidates[this.editedCandidateIndex].candidateName = this.beforeEditNameCache
+        this.editedCandidateIndex = null
+        this.editedCandidateIndex = null
+    }
 
     // FEATURE 12. A calculation across many parts.
     getLeadingCandidate () {
@@ -76,13 +105,7 @@ class Electorate {
     }
 }
 
-testElectorate = new Electorate("testElectorate")
-testElectorate.setNewCandidate("party1", "bob", 1000)
-testElectorate.setNewCandidate("party2", "steeve", 50)
-testElectorate.setNewCandidate("party3", "roberto", 1500)
-testElectorate.setNewCandidate("party4", "aaron", 1500)
-console.log(testElectorate.candidates)
-//test = testElectorate.sortCandidatesByVoteCount()
-//console.log(test)
-
-
+module.exports = {
+    Candidate : Candidate,
+    Electorate : Electorate
+}
